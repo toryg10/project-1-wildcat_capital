e62109d588ad75f6c297edec832865e10ca6c8bf
toryg10 <toryggauthier17@gmail.com>
2026-09-09 23:53:23 -0400

    Add virtual environment setup files

---
+++ verify_setup.py
@@ -0,0 +1,9 @@
+import pandas
+import numpy
+import matplotlib
+import jupyter_core
+
+print("pandas:", pandas.__version__)
+print("numpy:", numpy.__version__)
+print("matplotlib:", matplotlib.__version__)
+print("jupyter_core:", jupyter_core.__version__)
